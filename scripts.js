1442c2c874461f7aa7b958e79b4cc2cd5d083f8a
Isayah <218007520@tut4life.ac.za>
2024-12-16 18:18:26 +0200

    third again commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const aboutLinks = document.querySelectorAll(".links");
 const skillInfo =document.querySelector("#skill");
 const experienceInfo =document.querySelector("#experience");
 const educationInfo =document.querySelector("#education");
+const burgerMenu =document.querySelector(".bx-menu");
+const burDisapear = document.querySelector(".bx-x");
 
 experienceInfo.style.display = "none";
 educationInfo.style.display = "none";
@@ -26,4 +28,32 @@ aboutLinks.forEach((aboutLinks) => {
             experienceInfo.style.display = "none";
         }
     })
+})
+
+burgerMenu.addEventListener("click", function() {
+
+    document.querySelector(".bur-menu").style.display = "block";
+    burgerMenu.style.display = "none";
+
+    window.addEventListener("resize", function() {
+        const width = window.innerWidth;
+
+         if(width > 539)
+         {
+            document.querySelector(".bur-menu").style.display = "none";
+            burgerMenu.style.display = "none";
+
+         }
+         else if(width < 539){
+            document.querySelector(".bur-menu").style.display = "none";
+            burgerMenu.style.display = "block";
+         }
+      });
+   
+
+});
+
+burDisapear.addEventListener("click", function() {
+    document.querySelector(".bur-menu").style.display = "none";
+     burgerMenu.style.display = "block";
 })
